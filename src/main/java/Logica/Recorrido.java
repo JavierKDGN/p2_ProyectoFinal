63f5bd24fd7b93b93961c5c2f31000ecbb5d282e
JKdagan <javier.cadagan6@gmail.com>
2023-12-10 03:03:32 -0300

    Recorrido toString reorganizado

--- src/main/java/Logica/Recorrido.java
+++ src/main/java/Logica/Recorrido.java
@@ -79,7 +79,7 @@ public class Recorrido implements Comparable<Recorrido> {
         return tarifa;
     }
     public String toString() {
-        return hora_salida + "-" + hora_llegada + " " + origen + "-" + destino + "~ " + bus.toString() ;
+        return bus.toString() + " ~ " +hora_salida + "-" + hora_llegada + " " + origen + "-" + destino ;
     }
 
     public int compareTo(Recorrido recorrido) {
